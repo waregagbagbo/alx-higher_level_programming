36fb1db77ce8a19d1d80b108c3e725daab957342
waregagbagbo <gabelwarega@gmail.com>
2022-06-02 17:08:54 +0300

    file 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,35 +1,10 @@
 #!/usr/bin/py0hon3
-import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-"""def add(a, b):
-    a = 10
-    b = 5
 
-    return a + b
-
-def sub(a, b):
-    a = 10
-    b = 5
-
-    return a - b
-
-def mul(a, b):
-    a = 10
-    b = 5
-
-    return a * b
-
-
-def div(a, b):
-
-    a = 10
-    b = 5
-
-    return a / b
-"""
-print(calc.add(a, b))
-print(calc.div(a, b))
-print(calc.mul(a,b))
-print(calc.div(a, b))
+print(add(a, b))
+print(div(a, b))
+print(mul(a,b))
+print(div(a, b))
 
