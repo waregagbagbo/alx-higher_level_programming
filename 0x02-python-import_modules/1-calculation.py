0cbae49011067a711b3d1dd181d581806be54ada
waregagbagbo <gabelwarega@gmail.com>
2022-06-02 16:57:01 +0300

    addition of values

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,33 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/py0hon3
 import calculator_1
-def add():
+def add(a, b):
     a = 10
     b = 5
 
     return a + b
 
-def sub():
+def sub(a, b):
     a = 10
     b = 5
 
     return a - b
 
-def mul():
+def mul(a, b):
     a = 10
     b = 5
 
     return a * b
 
 
-def div():
+def div(a, b):
 
     a = 10
     b = 5
 
     return a / b
 
-print(add())
-print(sub())
-print(mul())
-print(div())
+print(add(0, 0))
+print(sub(0, 0))
+print(mul(0, 0))
+print(div(0, 0))
 
